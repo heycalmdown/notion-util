7dc16e43e78494b10da8b29889d8b1efdf9d7c75
heycalmdown <heyacct@gmail.com>
2019-12-31 22:39:03 +0900

    메모 쓰기 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NotionAgent } from 'notionapi-agent';
 import * as url from 'url';
 import * as _ from 'lodash';
+import { v4 } from 'uuid';
 
 const { NOTION_TOKEN } = process.env;
 
@@ -68,6 +69,16 @@ async function findCollectionIds() {
   return [collectionId, collectionViewId];
 }
 
+async function findNotesIds() {
+  const tempUrl = 'https://www.notion.so/kekefam/80f1b4ba615949faa9625bc42c5fb531?v=c1d00e9c432347c189b0055c24722312';
+  const id = getPageIDFromNotionPageURL(tempUrl);
+  const page = await agent.loadPageChunk(id)
+
+  const collectionId = _.keys(page.data.recordMap.collection)[0];
+  const collectionViewId = _.keys(page.data.recordMap.collection_view)[0];
+  return [collectionId, collectionViewId];
+}
+
 async function queryBooks(searchTerm: string) {
   const [collectionId, collectionViewId] = await findCollectionIds();
 
@@ -90,27 +101,145 @@ async function queryBooks(searchTerm: string) {
   return results.map(r => [r.value.id, r.value.properties.title[0][0], JSON.stringify(r.value.properties[keyByProperty['Read at']])]);
 }
 
-async function main() {
-  const searchTerm = process.argv[2] || '';
-  const results = await queryBooks(searchTerm);
-  console.log(results);
-  if (results.length !== 1) {
-    console.log('not exact match');
-    return;
-  }
+async function queryNotes() {
+  return findNotesIds();
+}
 
+async function updateReadAt(id: string) {
   const now = new Date();
   const timezoneShift = new Date(+now + 9 * 60 * 60 * 1000);
 
   const todayYYYYMMDD = timezoneShift.toISOString().split('T')[0];
 
   await agent.submitTransaction([{
-    id: results[0][0],
+    id,
     table: 'block',
     path: ['properties', 'fz`,'],
     command: 'set',
     args: [['‣',[['d',{'type':'date','start_date': todayYYYYMMDD}]]]]
   }]);
+}
+
+async function createNewDay(pageTitle: string) {
+  console.log('createNewDay');
+
+  const id = v4();
+
+  const args = {
+    id,
+    version: 1,
+    type: 'page',
+    alive: true,
+    properties: {
+      title: [[pageTitle]]
+    },
+    parent_id: '54cbdff5-ae05-439f-a3cd-e8513a449238',
+    parent_table: 'collection',
+    created_time: +new Date()
+  };
+
+  const result = await agent.submitTransaction([{
+    id,
+    table: 'block',
+    path: [],
+    command: 'set',
+    args: args as any
+  }]);
+
+  console.log(result);
+}
+
+async function addNewMemo(pageId: string, text: string) {
+  const id = v4();
+
+  const args = {
+    id,
+    type: 'text',
+    alive: true,
+    properties: {
+      title: [[text]]
+    },
+    parent_id: '678bf8f7-dbd6-4f11-86a3-da4aefd61e0a',
+    parent_table: 'block',
+    created_time: +new Date()
+  };
+
+  const result = await agent.submitTransaction([{
+    id,
+    table: 'block',
+    path: [],
+    command: 'set',
+    args: args as any
+  }, {
+    id: pageId,
+    table: 'block',
+    path: ['content'],
+    command: 'listAfter',
+    args: {
+      id
+    } as any
+  }]);
+
+  console.log(result);
+}
+
+async function getDailySeedBed() {
+  const [collectionId, collectionViewId] = await queryNotes();
+  console.time('queryCollection');
+  const response = await agent.queryCollection(collectionId, collectionViewId, []);
+  console.timeEnd('queryCollection');
+
+  const { block } = response.data.recordMap;
+  return block;
+}
+
+async function getToday(today: string) {
+  console.time('getDailySeedBed');
+  const block = await getDailySeedBed();
+  console.timeEnd('getDailySeedBed');
+
+  const blocks = _.values(block);
+  const pages = blocks.filter(b => b.value.type === 'page');
+  const results = pages.filter(p => p.value.properties.title[0][0] === today);
+  return results[0];
+}
+
+async function ensureToday() {
+  const now = new Date();
+  const timezoneShift = new Date(+now + 9 * 60 * 60 * 1000);
+
+  const todayYYYYMMDD = timezoneShift.toISOString().split('T')[0];
+
+  const result = await getToday(todayYYYYMMDD);
+  if (result) return result;
+
+  await createNewDay(todayYYYYMMDD);
+
+  return getToday(todayYYYYMMDD);
+}
+
+async function memo(text: string) {
+  const today = await ensureToday();
+  console.time('addNewMemo');
+  await addNewMemo(today.value.id, text);
+  console.timeEnd('addNewMemo');
+}
+
+async function main() {
+  const [command, ...args] = process.argv.slice(2);
+  if (command === 'bu') {
+    const [searchTerm] = args;
+    const results = await queryBooks(searchTerm);
+    console.log(results);
+    if (results.length !== 1) {
+      console.log('not exact match');
+      return;
+    }
+
+    await updateReadAt(results[0][0]);
+  } else if (command === 'dn') {
+    await memo(args.join(' '));
+  }
 }
 
 main();
